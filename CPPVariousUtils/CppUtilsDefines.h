44c3b4bff17046f7574ccd3f688e06cc95535b1c
michael@roop.ca <michael@roop.ca>
2021-12-31 17:06:58 -0400

    Commented out template exports

--- CPPVariousUtils/CppUtilsDefines.h
+++ CPPVariousUtils/CppUtilsDefines.h
@@ -30,25 +30,25 @@
 #endif
 
 
-//// Force export of the std char and wchar_t string and vector
-#if defined(WIN)
-   //disable warnings on 255 char debug symbols
-	#pragma warning (disable : 4786)
-	//disable warnings on extern before template instantiation
-	#pragma warning (disable : 4231)
-
-	// The order is important. Do not change
-	EXP_TEMPLATE template class CPPUTILS_API std::allocator<char>;
-	EXP_TEMPLATE template class CPPUTILS_API std::basic_string<char, struct std::char_traits<char>, std::allocator<char>>;
-	EXP_TEMPLATE template class CPPUTILS_API std::allocator< std::basic_string<char, struct std::char_traits<char>, std::allocator<char> > >;
-	EXP_TEMPLATE template class CPPUTILS_API std::vector< std::basic_string<char, struct std::char_traits<char>, std::allocator<char> > >;
-
-
-	EXP_TEMPLATE template class CPPUTILS_API std::allocator<wchar_t>;
-	EXP_TEMPLATE template class CPPUTILS_API std::basic_string<wchar_t, struct std::char_traits<wchar_t>, std::allocator<wchar_t> >;
-	EXP_TEMPLATE template class CPPUTILS_API std::allocator< std::basic_string<wchar_t, struct std::char_traits<wchar_t>, std::allocator<wchar_t> > >;
-	EXP_TEMPLATE template class CPPUTILS_API std::vector< std::basic_string<wchar_t, struct std::char_traits<wchar_t>, std::allocator<wchar_t> > >;
-#endif
+////// Force export of the std char and wchar_t string and vector
+//#if defined(WIN)
+//   //disable warnings on 255 char debug symbols
+//	#pragma warning (disable : 4786)
+//	//disable warnings on extern before template instantiation
+//	#pragma warning (disable : 4231)
+//
+//	//// The order is important. Do not change
+//	//EXP_TEMPLATE template class CPPUTILS_API std::allocator<char>;
+//	//EXP_TEMPLATE template class CPPUTILS_API std::basic_string<char, struct std::char_traits<char>, std::allocator<char>>;
+//	//EXP_TEMPLATE template class CPPUTILS_API std::allocator< std::basic_string<char, struct std::char_traits<char>, std::allocator<char> > >;
+//	//EXP_TEMPLATE template class CPPUTILS_API std::vector< std::basic_string<char, struct std::char_traits<char>, std::allocator<char> > >;
+//
+//
+//	//EXP_TEMPLATE template class CPPUTILS_API std::allocator<wchar_t>;
+//	//EXP_TEMPLATE template class CPPUTILS_API std::basic_string<wchar_t, struct std::char_traits<wchar_t>, std::allocator<wchar_t> >;
+//	//EXP_TEMPLATE template class CPPUTILS_API std::allocator< std::basic_string<wchar_t, struct std::char_traits<wchar_t>, std::allocator<wchar_t> > >;
+//	//EXP_TEMPLATE template class CPPUTILS_API std::vector< std::basic_string<wchar_t, struct std::char_traits<wchar_t>, std::allocator<wchar_t> > >;
+//#endif
 
 // TODO - figure out a way to get the string npos exported. It will cause link failures in with classes using the npos or substring
 
